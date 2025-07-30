3a2e7723dbb9e34b41679aa66ae4caa300575446
姜春 <perfect-jiangch@163.com>
2025-07-30 11:25:34 +0800

    测试6

--- scriptx.js
+++ scriptx.js
@@ -5,7 +5,7 @@ var webUrl = protocol + "//" + host + "/";
 var initCss = [];
 //初始化页面加载公共js脚本
 var initScripts = [
-        "custom/require.min.js",
+        "custom/sysinfo.js",
         "script/require/require.min.js"
     ];
 //****************************************************************************************************************************************************
